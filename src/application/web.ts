d8c160b759a8d729c8fea35661a6499a370b3c5e
Anggyar Muhamad Yahya <anggyar.0106@gmail.com>
2025-08-12 18:33:04 +0700

    implement user registration API with validation and error handling

--- src/application/web.ts
+++ src/application/web.ts
@@ -1,4 +1,8 @@
 import express from "express";
+import { publicRouter } from "../router/public-api";
+import { errorMidlleware } from "../middleware/error-middleware";
 
-export const app = express();
-app.use(express.json());
+export const web = express();
+web.use(express.json());
+web.use(publicRouter);
+web.use(errorMidlleware);
---
+++ src/router/public-api.ts
@@ -0,0 +1,7 @@
+import express from "express";
+import { UserController } from "../controller/user-controller";
+
+export const publicRouter = express.Router();
+
+// Belum login
+publicRouter.post("/api/users", UserController.register);
