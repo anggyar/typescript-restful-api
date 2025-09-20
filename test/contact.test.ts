9e786bfaf1e4700ff95840b1ad2c5d73704cf848
Anggyar Muhamad Yahya <anggyar.0106@gmail.com>
2025-09-21 03:08:11 +0700

    fix typo and refactor unit test for contact

--- test/contact.test.ts
+++ test/contact.test.ts
@@ -2,258 +2,266 @@ import supertest from "supertest";
 import { ContactTest, UserTest } from "./test-utils";
 import { web } from "../src/application/web";
 import { logger } from "../src/application/logging";
-import { response } from "express";
-import { email } from "zod";
 
 describe("POST /api/contacts", () => {
-    beforeEach(async () => {
-        await UserTest.create();
-    });
+  beforeEach(async () => {
+    await UserTest.create();
+  });
 
-    afterEach(async () => {
-        await ContactTest.deleteAll();
-        await UserTest.delete();
-    });
+  afterEach(async () => {
+    await ContactTest.deleteAll();
+    await UserTest.delete();
+  });
 
-    it("should create new contact", async () => {
-        const response = await supertest(web).post("/api/contacts").set("X-API-TOKEN", "test").send({
-            first_name: "Anggyar",
-            last_name: "Muhamad Yahya",
-            email: "anggyar@mail.com",
-            phone: "08900112233",
-        });
+  it("should create new contact", async () => {
+    const response = await supertest(web)
+      .post("/api/contacts")
+      .set("X-API-TOKEN", "test")
+      .send({
+        first_name: "Anggyar",
+        last_name: "Muhamad Yahya",
+        email: "anggyar@mail.com",
+        phone: "08900112233",
+      });
 
-        logger.debug(response.body);
-        expect(response.status).toBe(200);
-        expect(response.body.data.id).toBeDefined();
-        expect(response.body.data.first_name).toBe("Anggyar");
-        expect(response.body.data.last_name).toBe("Muhamad Yahya");
-        expect(response.body.data.email).toBe("anggyar@mail.com");
-        expect(response.body.data.phone).toBe("08900112233");
-    });
+    logger.debug(response.body);
+    expect(response.status).toBe(200);
+    expect(response.body.data.id).toBeDefined();
+    expect(response.body.data.first_name).toBe("Anggyar");
+    expect(response.body.data.last_name).toBe("Muhamad Yahya");
+    expect(response.body.data.email).toBe("anggyar@mail.com");
+    expect(response.body.data.phone).toBe("08900112233");
+  });
 
-    it("should reject create new contact if data is invalid", async () => {
-        const response = await supertest(web).post("/api/contacts").set("X-API-TOKEN", "test").send({
-            first_name: "",
-            last_name: "",
-            email: "anggyar",
-            phone: "0890011223383012313802348028",
-        });
+  it("should reject create new contact if data is invalid", async () => {
+    const response = await supertest(web)
+      .post("/api/contacts")
+      .set("X-API-TOKEN", "test")
+      .send({
+        first_name: "",
+        last_name: "",
+        email: "anggyar",
+        phone: "0890011223383012313802348028",
+      });
 
-        logger.debug(response.body);
-        expect(response.status).toBe(400);
-        expect(response.body.errors).toBeDefined();
-    });
+    logger.debug(response.body);
+    expect(response.status).toBe(400);
+    expect(response.body.errors).toBeDefined();
+  });
 });
 
 describe("Get /api/contacts/:contactId", () => {
-    beforeEach(async () => {
-        await UserTest.create();
-        await ContactTest.create();
-    });
+  beforeEach(async () => {
+    await UserTest.create();
+    await ContactTest.create();
+  });
 
-    afterEach(async () => {
-        await ContactTest.deleteAll();
-        await UserTest.delete();
-    });
+  afterEach(async () => {
+    await ContactTest.deleteAll();
+    await UserTest.delete();
+  });
 
-    it("should be able get contact", async () => {
-        const contact = await ContactTest.get();
-        const response = await supertest(web).get(`/api/contacts/${contact.id}`).set("X-API-TOKEN", "test");
+  it("should be able get contact", async () => {
+    const contact = await ContactTest.get();
+    const response = await supertest(web)
+      .get(`/api/contacts/${contact.id}`)
+      .set("X-API-TOKEN", "test");
 
-        logger.debug(response.body);
-        expect(response.status).toBe(200);
-        expect(response.body.data.id).toBeDefined();
-        expect(response.body.data.first_name).toBe(contact.first_name);
-        expect(response.body.data.last_name).toBe(contact.last_name);
-        expect(response.body.data.email).toBe(contact.email);
-    });
+    logger.debug(response.body);
+    expect(response.status).toBe(200);
+    expect(response.body.data.id).toBeDefined();
+    expect(response.body.data.first_name).toBe(contact.first_name);
+    expect(response.body.data.last_name).toBe(contact.last_name);
+    expect(response.body.data.email).toBe(contact.email);
+  });
 
-    it("should reject get contact if contact is not found", async () => {
-        const contact = await ContactTest.get();
-        const response = await supertest(web)
-            .get(`/api/contacts/${contact.id + 1}`)
-            .set("X-API-TOKEN", "test");
+  it("should reject get contact if contact is not found", async () => {
+    const contact = await ContactTest.get();
+    const response = await supertest(web)
+      .get(`/api/contacts/${contact.id + 1}`)
+      .set("X-API-TOKEN", "test");
 
-        logger.debug(response.body);
-        expect(response.status).toBe(404);
-        expect(response.body.errors).toBeDefined();
-    });
+    logger.debug(response.body);
+    expect(response.status).toBe(404);
+    expect(response.body.errors).toBeDefined();
+  });
 });
 
 describe("PUT /api/contacts/:contactId", () => {
-    beforeEach(async () => {
-        await UserTest.create();
-        await ContactTest.create();
-    });
+  beforeEach(async () => {
+    await UserTest.create();
+    await ContactTest.create();
+  });
 
-    afterEach(async () => {
-        await ContactTest.deleteAll();
-        await UserTest.delete();
-    });
+  afterEach(async () => {
+    await ContactTest.deleteAll();
+    await UserTest.delete();
+  });
 
-    it("should be able to update contact", async () => {
-        const contact = await ContactTest.get();
-        const response = await supertest(web)
-            .put(`/api/contacts/${contact.id}`)
-            .set("X-API-TOKEN", "test")
-            .send({
-                first_name: "anggyar",
-                last_name: "muhamad yahya",
-                email: "anggyar@mail.com",
-                phone: "09999",
-            });
+  it("should be able to update contact", async () => {
+    const contact = await ContactTest.get();
+    const response = await supertest(web)
+      .put(`/api/contacts/${contact.id}`)
+      .set("X-API-TOKEN", "test")
+      .send({
+        first_name: "anggyar",
+        last_name: "muhamad yahya",
+        email: "anggyar@mail.com",
+        phone: "09999",
+      });
 
-        logger.debug(response.body);
-        expect(response.status).toBe(200);
-        expect(response.body.data.id).toBe(contact.id);
-        expect(response.body.data.first_name).toBe("anggyar");
-        expect(response.body.data.last_name).toBe("muhamad yahya");
-        expect(response.body.data.email).toBe("anggyar@mail.com");
-        expect(response.body.data.phone).toBe("09999");
-    });
+    logger.debug(response.body);
+    expect(response.status).toBe(200);
+    expect(response.body.data.id).toBe(contact.id);
+    expect(response.body.data.first_name).toBe("anggyar");
+    expect(response.body.data.last_name).toBe("muhamad yahya");
+    expect(response.body.data.email).toBe("anggyar@mail.com");
+    expect(response.body.data.phone).toBe("09999");
+  });
 
-    it("should reject to update contact if request invalid", async () => {
-        const contact = await ContactTest.get();
-        const response = await supertest(web)
-            .put(`/api/contacts/${contact.id}`)
-            .set("X-API-TOKEN", "test")
-            .send({
-                first_name: "",
-                last_name: "m",
-                email: "anggyar",
-                phone: "",
-            });
+  it("should reject to update contact if request invalid", async () => {
+    const contact = await ContactTest.get();
+    const response = await supertest(web)
+      .put(`/api/contacts/${contact.id}`)
+      .set("X-API-TOKEN", "test")
+      .send({
+        first_name: "",
+        last_name: "m",
+        email: "anggyar",
+        phone: "",
+      });
 
-        logger.debug(response.body);
-        expect(response.status).toBe(400);
-        expect(response.body.errors).toBeDefined;
-    });
+    logger.debug(response.body);
+    expect(response.status).toBe(400);
+    expect(response.body.errors).toBeDefined;
+  });
 });
 
 describe("DELETE /api/contacts/:contactId", () => {
-    beforeEach(async () => {
-        await UserTest.create();
-        await ContactTest.create();
-    });
+  beforeEach(async () => {
+    await UserTest.create();
+    await ContactTest.create();
+  });
 
-    afterEach(async () => {
-        await ContactTest.deleteAll();
-        await UserTest.delete();
-    });
+  afterEach(async () => {
+    await ContactTest.deleteAll();
+    await UserTest.delete();
+  });
 
-    it("should be able to remove contact is not found", async () => {
-        const contact = await ContactTest.get();
-        const response = await supertest(web)
-            .delete(`/api/contacts/${contact.id + 1}`)
-            .set("X-API-TOKEN", "test");
+  it("should be able to remove contact is not found", async () => {
+    const contact = await ContactTest.get();
+    const response = await supertest(web)
+      .delete(`/api/contacts/${contact.id + 1}`)
+      .set("X-API-TOKEN", "test");
 
-        logger.debug(response);
-        expect(response.status).toBe(404);
-        expect(response.body.errors).toBeDefined();
-    });
+    logger.debug(response);
+    expect(response.status).toBe(404);
+    expect(response.body.errors).toBeDefined();
+  });
 });
 
 describe("GET /api/contacts", () => {
-    beforeEach(async () => {
-        await UserTest.create();
-        await ContactTest.create();
-    });
+  beforeEach(async () => {
+    await UserTest.create();
+    await ContactTest.create();
+  });
 
-    afterEach(async () => {
-        await ContactTest.deleteAll();
-        await UserTest.delete();
-    });
+  afterEach(async () => {
+    await ContactTest.deleteAll();
+    await UserTest.delete();
+  });
 
-    it("should be able to search contact", async () => {
-        const response = await supertest(web).get("/api/contacts").set("X-API-TOKEN", "test");
+  it("should be able to search contact", async () => {
+    const response = await supertest(web)
+      .get("/api/contacts")
+      .set("X-API-TOKEN", "test");
 
-        logger.debug(response.body);
-        expect(response.status).toBe(200);
-        expect(response.body.data.length).toBe(1);
-        expect(response.body.paging.current_page).toBe(1);
-        expect(response.body.paging.total_page).toBe(1);
-        expect(response.body.paging.size).toBe(10);
-    });
+    logger.debug(response.body);
+    expect(response.status).toBe(200);
+    expect(response.body.data.length).toBe(1);
+    expect(response.body.paging.current_page).toBe(1);
+    expect(response.body.paging.total_page).toBe(1);
+    expect(response.body.paging.size).toBe(10);
+  });
 
-    it("should be able to search contact using name", async () => {
-        const response = await supertest(web)
-            .get("/api/contacts")
-            .query({
-                name: "es",
-            })
-            .set("X-API-TOKEN", "test");
+  it("should be able to search contact using name", async () => {
+    const response = await supertest(web)
+      .get("/api/contacts")
+      .query({
+        name: "es",
+      })
+      .set("X-API-TOKEN", "test");
 
-        logger.debug(response.body);
-        expect(response.status).toBe(200);
-        expect(response.body.data.length).toBe(1);
-        expect(response.body.paging.current_page).toBe(1);
-        expect(response.body.paging.total_page).toBe(1);
-        expect(response.body.paging.size).toBe(10);
-    });
+    logger.debug(response.body);
+    expect(response.status).toBe(200);
+    expect(response.body.data.length).toBe(1);
+    expect(response.body.paging.current_page).toBe(1);
+    expect(response.body.paging.total_page).toBe(1);
+    expect(response.body.paging.size).toBe(10);
+  });
 
-    it("should be able to search contact using email", async () => {
-        const response = await supertest(web)
-            .get("/api/contacts")
-            .query({
-                email: ".com",
-            })
-            .set("X-API-TOKEN", "test");
+  it("should be able to search contact using email", async () => {
+    const response = await supertest(web)
+      .get("/api/contacts")
+      .query({
+        email: ".com",
+      })
+      .set("X-API-TOKEN", "test");
 
-        logger.debug(response.body);
-        expect(response.status).toBe(200);
-        expect(response.body.data.length).toBe(1);
-        expect(response.body.paging.current_page).toBe(1);
-        expect(response.body.paging.total_page).toBe(1);
-        expect(response.body.paging.size).toBe(10);
-    });
+    logger.debug(response.body);
+    expect(response.status).toBe(200);
+    expect(response.body.data.length).toBe(1);
+    expect(response.body.paging.current_page).toBe(1);
+    expect(response.body.paging.total_page).toBe(1);
+    expect(response.body.paging.size).toBe(10);
+  });
 
-    it("should be able to search contact using phone", async () => {
-        const response = await supertest(web)
-            .get("/api/contacts")
-            .query({
-                phone: "99",
-            })
-            .set("X-API-TOKEN", "test");
+  it("should be able to search contact using phone", async () => {
+    const response = await supertest(web)
+      .get("/api/contacts")
+      .query({
+        phone: "99",
+      })
+      .set("X-API-TOKEN", "test");
 
-        logger.debug(response.body);
-        expect(response.status).toBe(200);
-        expect(response.body.data.length).toBe(1);
-        expect(response.body.paging.current_page).toBe(1);
-        expect(response.body.paging.total_page).toBe(1);
-        expect(response.body.paging.size).toBe(10);
-    });
+    logger.debug(response.body);
+    expect(response.status).toBe(200);
+    expect(response.body.data.length).toBe(1);
+    expect(response.body.paging.current_page).toBe(1);
+    expect(response.body.paging.total_page).toBe(1);
+    expect(response.body.paging.size).toBe(10);
+  });
 
-    it("should be able to search contact no result", async () => {
-        const response = await supertest(web)
-            .get("/api/contacts")
-            .query({
-                name: "salah",
-            })
-            .set("X-API-TOKEN", "test");
+  it("should be able to search contact no result", async () => {
+    const response = await supertest(web)
+      .get("/api/contacts")
+      .query({
+        name: "salah",
+      })
+      .set("X-API-TOKEN", "test");
 
-        logger.debug(response.body);
-        expect(response.status).toBe(200);
-        expect(response.body.data.length).toBe(0);
-        expect(response.body.paging.current_page).toBe(1);
-        expect(response.body.paging.total_page).toBe(0);
-        expect(response.body.paging.size).toBe(10);
-    });
+    logger.debug(response.body);
+    expect(response.status).toBe(200);
+    expect(response.body.data.length).toBe(0);
+    expect(response.body.paging.current_page).toBe(1);
+    expect(response.body.paging.total_page).toBe(0);
+    expect(response.body.paging.size).toBe(10);
+  });
 
-    it("should be able to search contact with paging", async () => {
-        const response = await supertest(web)
-            .get("/api/contacts")
-            .query({
-                page: 2,
-                size: 1,
-            })
-            .set("X-API-TOKEN", "test");
+  it("should be able to search contact with paging", async () => {
+    const response = await supertest(web)
+      .get("/api/contacts")
+      .query({
+        page: 2,
+        size: 1,
+      })
+      .set("X-API-TOKEN", "test");
 
-        logger.debug(response.body);
-        expect(response.status).toBe(200);
-        expect(response.body.data.length).toBe(0);
-        expect(response.body.paging.current_page).toBe(2);
-        expect(response.body.paging.total_page).toBe(1);
-        expect(response.body.paging.size).toBe(1);
-    });
+    logger.debug(response.body);
+    expect(response.status).toBe(200);
+    expect(response.body.data.length).toBe(0);
+    expect(response.body.paging.current_page).toBe(2);
+    expect(response.body.paging.total_page).toBe(1);
+    expect(response.body.paging.size).toBe(1);
+  });
 });
